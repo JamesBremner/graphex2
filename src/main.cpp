bf7741c18e2b77e230eab5a0fde235fcb908c322
JamesBremner <james@ravenspoint.com>
2022-11-25 10:37:20 -0500

    Delete node

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,12 @@ class cNode
 {
 public:
     int x, y;
+    int myID;
+    static int myLastID;
+    cNode()
+    {
+        myID = ++myLastID;
+    }
     void loc(int X, int Y)
     {
         x = X;
@@ -25,45 +31,78 @@ public:
     {
         return (abs(x - X) < 10 && abs(y - Y) < 10);
     }
+    bool operator==(const cNode &other)
+    {
+        return (x == other.x && y == other.y);
+    }
 };
+
+int cNode::myLastID = 0;
+
 /// @brief a link between two nodes
 class cLink
 {
 public:
-    cNode &n1, &n2;
-    cLink(cNode &a, cNode &b)
+    int n1, n2; // IDs of nodes linked
+    cLink(int a, int b)
         : n1(a), n2(b)
     {
     }
 };
 
-/// @brief A graph pf nodes connected by links
+/// @brief A graph of nodes connected by links
 class cGraph
 {
 public:
     std::vector<cNode> vN; ///< the nodes
     std::vector<cLink> vL; ///< the links
     int selected;          ///< index of currently selected node
+    int prevSelected;
+
+    cGraph()
+        : selected(-1)
+    {
+    }
 
     cNode &addNode()
     {
         vN.push_back(cNode());
         return vN.back();
     }
-    void addLink(int n1, int n2)
-    {
-        vL.push_back(cLink(vN[n1], vN[n2]));
-    }
+
+    /// @brief Link selected and previously selected nodes
+    void link();
+
+    /// @brief node near location
+    /// @param x
+    /// @param y
+    /// @return near node index, -1 if none
+    int nearNode(int x, int y) const;
+
     /// @brief Select a node if there is one close to the mouse cursor
     /// @param x
     /// @param y
     void selectIfNear(int x, int y);
 
+    void select(int id)
+    {
+        prevSelected = selected;
+        selected = id;
+    }
+
     void SelectedNodeMove(int x, int y)
     {
         if (selected >= 0)
-            vN[selected].loc(x, y);
+            selectedNode().loc(x, y);
     }
+    void SelectedRemove();
+
+    bool isSelected() const
+    {
+        return selected >= 0;
+    }
+    cNode &findNode(int id);
+    cNode &selectedNode();
 };
 
 class cGUI : public cStarterGUI
@@ -95,22 +134,73 @@ private:
     void rightClick();
     void leftClick();
 
-    void newNode();
-    void link();
+    void remove();
 
     void draw(wex::shapes &S);
 };
 
-void cGraph::selectIfNear(int x, int y)
+cNode &cGraph::findNode(int id)
+{
+    for (auto &n : vN)
+    {
+        if (n.myID == id)
+            return n;
+    }
+    throw std::runtime_error("Cannot find node");
+}
+cNode &cGraph::selectedNode()
+{
+    if (selected < 0)
+        return vN[0];
+    return findNode(selected);
+}
+
+int cGraph::nearNode(int x, int y) const
 {
     for (int kn = 0; kn < vN.size(); kn++)
     {
         if (vN[kn].isNear(x, y))
         {
-            selected = kn;
-            return;
+            return vN[kn].myID;
         }
     }
+    return -1;
+}
+
+void cGraph::link()
+{
+    if (selected < 0 || prevSelected < 0 ||
+        selected == prevSelected)
+        return;
+    vL.push_back(cLink(selected, prevSelected));
+}
+
+void cGraph::selectIfNear(int x, int y)
+{
+    selected = nearNode(x, y);
+}
+
+void cGraph::SelectedRemove()
+{
+    if (selected < 0)
+        return;
+
+    // remove connected links
+    vL.erase(std::remove_if(
+                 vL.begin(), vL.end(),
+                 [this](cLink l)
+                 {
+                     return (l.n1 == selected || l.n2 == selected);
+                 }),
+             vL.end());
+
+    vN.erase(std::remove_if(
+                 vN.begin(), vN.end(),
+                 [this](cNode n)
+                 {
+                     return (n.myID == selected);
+                 }),
+             vN.end());
     selected = -1;
 }
 
@@ -121,15 +211,18 @@ void cGUI::draw(wex::shapes &S)
     for (int kl = 0; kl < G.vL.size(); kl++)
     {
         auto &l = G.vL[kl];
-        S.line({l.n1.x, l.n1.y,
-                l.n2.x, l.n2.y});
+        auto &n1 = G.findNode(l.n1);
+        auto &n2 = G.findNode(l.n2);
+        S.line({n1.x, n1.y,
+                n2.x, n2.y});
     }
     // draw nodes
     for (int kn = 0; kn < G.vN.size(); kn++)
     {
         cNode &n = G.vN[kn];
+        // std::cout << n.myID << " " << n.x << " " << n.y << "\n";
         S.color(0x000000);
-        if (kn == G.selected)
+        if (n.myID == G.selected)
             S.color(0x0000FF);
         S.circle(n.x, n.y, 10);
     }
@@ -169,13 +262,19 @@ void cGUI::registerHandlers()
 }
 void cGUI::ConstructMenu()
 {
-    int clicked;
     mRightClick = new wex::menu(fm);
-    mRightClick->append("New Node",
+    mRightClick->append("Link with selected",
                         [&](const std::string &title)
-                        { newNode(); });
-    mRightClick->append("Link with selected", [&](const std::string &title)
-                        { link(); });
+                        {
+                            G.link();
+                            fm.update();
+                        });
+    mRightClick->append("Delete",
+                        [&](const std::string &title)
+                        {
+                            G.SelectedRemove();
+                            fm.update();
+                        });
 }
 
 void cGUI::leftClick()
@@ -188,29 +287,22 @@ void cGUI::leftClick()
 void cGUI::rightClick()
 {
     mouseStatus = fm.getMouseStatus();
+    std::cout << mouseStatus.x << " " << mouseStatus.y << "\n";
+    int nodeClicked = G.nearNode(mouseStatus.x, mouseStatus.y);
+    if (nodeClicked == -1)
+    {
+        // click not near a node
+        // add new node at click location
+        auto &n = G.addNode();
+        n.loc(mouseStatus.x, mouseStatus.y);
+        G.select(n.myID);
+        fm.update();
+        return;
+    }
+
+    // click on node, popup menu
+    G.select(nodeClicked);
     mRightClick->popup(mouseStatus.x, mouseStatus.y);
-}
-
-void cGUI::newNode()
-{
-    auto &n = G.addNode();
-    n.loc(mouseStatus.x, mouseStatus.y);
-    fm.update();
-}
-
-void cGUI::link()
-{
-    if (G.selected < 0)
-        return;
-    int link = G.selected;
-
-    G.selectIfNear(mouseStatus.x, mouseStatus.y);
-    if (G.selected < 0)
-        return;
-    if (G.selected == link)
-        return;
-    G.addLink(link, G.selected);
-    fm.update();
 }
 
 main()
